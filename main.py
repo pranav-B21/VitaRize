4565a8bcc3231f994a19f55378e444e193b76b64
Pranav <pranav.belligundu@gmail.com>
2023-07-13 11:19:05 -0700

    more stuff. api key gets disabled

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-# Import the required libraries
 import openai
-# import os
 import streamlit as st
 from streamlit_option_menu import option_menu
-from langchain.document_loaders import PyPDFLoader
-from langchain import PromptTemplate
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.chains.summarize import load_summarize_chain
+import PyPDF2
 
 # Set the GPT-3 API key
 openai.api_key = st.secrets["pass"]
@@ -21,45 +16,53 @@ selected_page = option_menu(
 )
 
 
-def generate_summary(text, user_prompt):
-    if st.button("Generate Summary", type='primary'):
-        # Use GPT-3 to generate a summary of the article
-        response = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt="This is the user prompt for how the text should be summarized" +
-            user_prompt + "here is the text:" + text,
-            temperature=0.5,
-        )
-        # Print the generated summary
-        res = response["choices"][0]["text"]
-        st.success(res)
-        st.download_button('Download result', res)
+def generate_summary(text, temperature, length):
+    # Use GPT-3 to generate a summary of the article
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt="This is the user prompt for how the text should be summarized" +
+        "here is the text:" + text,
+        temperature=temperature,
+        max_tokens=length,
+    )
+    # Print the generated summary
+    res = response["choices"][0]["text"]
+    st.success(res)
+    st.download_button('Download result', res)
 
 
 def extract_text_from_pdf(file):
-    print("hi")
+    # Read the PDF file
+    pdf_reader = PyPDF2.PdfReader(file)
+
+    # Extract text from each page
+    text = ""
+    for page_num in range(len(pdf_reader.pages)):
+        page = pdf_reader.pages[page_num]
+        text += page.extract_text()
+
+    # Close the PDF file
+    file.close()
+
+    return text
 
 
 if selected_page == "Summarizer":
     st.title("PDF Summarization")
-    st.write("Upload a PDF document and enter a prompt. Then click 'Generate Summary' to generate a personalized summary.")
-
     # Upload PDF file
     uploaded_file = st.file_uploader("Upload a PDF file", type="pdf")
-
-    # Custom Prompt
-    prompt = st.text_input("Enter a prompt")
 
     if uploaded_file is not None:
         # Convert PDF to text
         text = extract_text_from_pdf(uploaded_file)
 
-        # Display original text
-        st.header("Original Text")
-        st.write(text)
+        # Prompt settings
+        temperature = st.slider(
+            "Temperature", min_value=0.0, max_value=1.0, step=0.1, value=0.5)
+        length = st.slider("Length of Summary", min_value=50,
+                           max_value=500, step=50, value=100)
 
-        # Generate summary
-        if prompt:
-            summary = generate_summary(text, prompt)
+        if st.button("Generate Summary"):
+            summary = generate_summary(text, temperature, length)
             st.header("Summary")
             st.write(summary)
