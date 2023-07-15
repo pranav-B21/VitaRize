a6039d79f906833fdafdefdeb82dd8fa4b433f2d
Diya <diyapendyala@gmail.com>
2023-07-14 23:08:39 -0700

    img and pdf scraping fixed

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import PyPDF2
 from transformers import pipeline
 import pandas as pd
 import altair as alt
+import pytesseract
+from PIL import Image
 
 
 # pip install openai
@@ -31,7 +33,6 @@ selected_page = option_menu(
     orientation="horizontal",
 )
 
-
 def generate_summary(text, temperature, length, user_prompt):
     # Use GPT-3 to generate a summary of the article
     response = openai.Completion.create(
@@ -46,6 +47,12 @@ def generate_summary(text, temperature, length, user_prompt):
     st.success(res)
     st.download_button('Download result', res)
 
+
+def extract_text_from_img(file):
+    # Convert the image into a string
+    text = str(((pytesseract.image_to_string(Image.open(file)))))
+    text = text.replace("-\n", "")
+    return text
 
 def extract_text_from_pdf(file):
     # Read the PDF file
@@ -62,7 +69,6 @@ def extract_text_from_pdf(file):
 
     return text
 
-
 def predict_sentiment(text):
     classifier = pipeline("sentiment-analysis")
     result = classifier(user_input)[0]
@@ -70,14 +76,28 @@ def predict_sentiment(text):
 
 
 if selected_page == "Summarizer":
-    st.title("PDF Analyzer")
+    st.title("Document Analyzer")
     # Upload PDF file
-    uploaded_file = st.file_uploader("Upload a PDF file", type="pdf")
-
+    uploaded_file = st.file_uploader("Upload a PDF file", type=['pdf'])
+    uploaded_image = st.file_uploader("Upload a PNG file", type=['png'])
     if uploaded_file is not None:
         # Convert PDF to text
         text = extract_text_from_pdf(uploaded_file)
+        # Prompt settings
+        temperature = st.slider(
+            "Temperature", min_value=0.0, max_value=1.0, step=0.1, value=0.5)
+        length = st.slider("Length of Summary", min_value=50,
+                           max_value=500, step=50, value=100)
+        user_prompt = st.text_input(
+            "Ask questions about this file")
 
+        if st.button("Generate Summary"):
+            summary = generate_summary(text, temperature, length, user_prompt)
+            st.header("Summary")
+            st.write(summary)
+    if uploaded_image is not None:
+        # Convert PNG to text
+        text = extract_text_from_img(uploaded_image)
         # Prompt settings
         temperature = st.slider(
             "Temperature", min_value=0.0, max_value=1.0, step=0.1, value=0.5)
@@ -92,16 +112,20 @@ if selected_page == "Summarizer":
             st.write(summary)
 
 if selected_page == "Sentiment":
+    print("sentiment")
     st.title("Sentiment Analyzer")
     st.write(
-        'Upload your file or write some text. The app will predict its sentiment.')
-    uploaded_file = st.file_uploader("Upload a PDF file", type="pdf")
+        'Upload your file (either a PNG or PDF) or write some text. The app will predict its sentiment.')
+    uploaded_file = st.file_uploader("Upload a PDF file", type=['pdf'])
+    uploaded_image = st.file_uploader("Upload a PNG file", type=['png'])
     user_input = st.text_input("Enter your text here")
     text = ":)"
 
     if uploaded_file is not None:
         # Convert PDF to text
         text = extract_text_from_pdf(uploaded_file)
+    elif uploaded_image is not None:
+        text = extract_text_from_img(uploaded_image)
     elif user_input:
         text = user_input
 
