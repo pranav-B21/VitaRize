bfb033b071c1332aa8f212a643b8778bdc9f854c
Pranav <pranav.belligundu@gmail.com>
2023-07-16 15:37:45 -0700

    text to speech

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import openai
 import streamlit as st
-from streamlit_option_menu import option_menu
 import PyPDF2
-from transformers import pipeline
 import pandas as pd
 import altair as alt
 import pytesseract
-from PIL import Image
+import pyttsx3 as engine
+import io
 
+
+from PIL import Image
+from gtts import gTTS
+from streamlit_option_menu import option_menu
+from streamlit_chat import message
+from transformers import pipeline
 
 # pip install openai
 # pip install streamlit
 # pip install PyPDF2
 # pip install transformers
 # pip install torch
+# pip install pyttsx3
 # brew install poppler-qt5
 # pip install pytesseract / brew install tesseract (i would do both to be safe)
 # once you get a new api key, go to secrets.toml and add it instead of pass
@@ -25,7 +31,7 @@ openai.api_key = st.secrets["pass"]
 
 sentiment_score = {}
 
-logo = "Design.png"
+logo = "Design (2).png"
 st.image(logo, use_column_width=True)
 
 selected_page = option_menu(
@@ -37,12 +43,12 @@ selected_page = option_menu(
     orientation="horizontal",
 )
 
-def generate_summary(text, temperature, length, user_prompt):
+
+def generate_summary(text, temperature, length):
     # Use GPT-3 to generate a summary of the article
     response = openai.Completion.create(
         engine="text-davinci-002",
-        prompt="What do you want to learn from this paper? " +
-        user_prompt + " Here is the text:" + text,
+        prompt="Summatize this paper " + text,
         temperature=temperature,
         max_tokens=length,
     )
@@ -51,12 +57,19 @@ def generate_summary(text, temperature, length, user_prompt):
     st.success(res)
     st.download_button('Download result', res)
 
+    # Convert summary to speech
+    tts = gTTS(text=res, lang='en', slow=False)
+    audio_file = io.BytesIO()
+    tts.write_to_fp(audio_file)
+    st.audio(audio_file, format='audio/mp3')
+
 
 def extract_text_from_img(file):
     # Convert the image into a string
     text = str(((pytesseract.image_to_string(Image.open(file)))))
     text = text.replace("-\n", "")
     return text
+
 
 def extract_text_from_pdf(file):
     # Read the PDF file
@@ -72,6 +85,7 @@ def extract_text_from_pdf(file):
     file.close()
 
     return text
+
 
 def predict_sentiment(text):
     classifier = pipeline("sentiment-analysis")
@@ -92,13 +106,10 @@ if selected_page == "Summarizer":
             "Temperature", min_value=0.0, max_value=1.0, step=0.1, value=0.5)
         length = st.slider("Length of Summary", min_value=50,
                            max_value=500, step=50, value=100)
-        user_prompt = st.text_input(
-            "Ask questions about this file")
 
         if st.button("Generate Summary"):
-            summary = generate_summary(text, temperature, length, user_prompt)
-            st.header("Summary")
-            st.write(summary)
+            generate_summary(text, temperature, length)
+
     if uploaded_image is not None:
         # Convert PNG to text
         text = extract_text_from_img(uploaded_image)
@@ -107,13 +118,10 @@ if selected_page == "Summarizer":
             "Temperature", min_value=0.0, max_value=1.0, step=0.1, value=0.5)
         length = st.slider("Length of Summary", min_value=50,
                            max_value=500, step=50, value=100)
-        user_prompt = st.text_input(
-            "Ask questions about this file")
 
         if st.button("Generate Summary"):
-            summary = generate_summary(text, temperature, length, user_prompt)
-            st.header("Summary")
-            st.write(summary)
+            generate_summary(text, temperature, length)
+
 
 if selected_page == "Sentiment":
     print("sentiment")
@@ -144,4 +152,4 @@ if selected_page == "Home":
     st.title("Home Page")
     st.write(
         "Welcome to VitaRize! This application is a customized document summarizer that you can use for anything!")
-    st.write("This was made with OpenAI API and Langchain")
+    st.write("This was made with OpenAI API and Altair")
